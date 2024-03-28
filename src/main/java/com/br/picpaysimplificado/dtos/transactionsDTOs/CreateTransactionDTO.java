beb44968b21701ab56731b79eb5fed45d4b83121
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 18:27:09 -0300

    CreateTransação DTO - novo dto

---
+++ src/main/java/com/br/picpaysimplificado/dtos/transactionsDTOs/CreateTransactionDTO.java
@@ -0,0 +1,16 @@
+package com.br.picpaysimplificado.dtos.transactionsDTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record CreateTransactionDTO(
+        @NotNull
+        BigDecimal value,
+        @NotBlank
+        String cpfSender,
+        @NotBlank
+        String cpfReceiver
+) {
+}
