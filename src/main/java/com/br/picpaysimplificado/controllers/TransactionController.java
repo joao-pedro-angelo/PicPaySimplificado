c3888fea622f2ee5aa849e6231f1a8cf252f2823
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 18:41:33 -0300

    Controller de transações

---
+++ src/main/java/com/br/picpaysimplificado/controllers/TransactionController.java
@@ -0,0 +1,29 @@
+package com.br.picpaysimplificado.controllers;
+
+import com.br.picpaysimplificado.domain.transactions.Transaction;
+import com.br.picpaysimplificado.dtos.transactionsDTOs.CreateTransactionDTO;
+import com.br.picpaysimplificado.dtos.transactionsDTOs.GetTransactionDTO;
+import com.br.picpaysimplificado.services.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService service;
+
+    @PostMapping
+    public ResponseEntity<GetTransactionDTO> createTransaction(
+            @RequestBody @Valid CreateTransactionDTO createTransactionDTO){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(this.service.createTransaction(createTransactionDTO));
+    }
+}
