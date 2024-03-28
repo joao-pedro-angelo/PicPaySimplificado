c3a10865ce4bf425f648712ef4115677a53043b0
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 17:44:54 -0300

    cria entidades da transação

---
+++ src/main/java/com/br/picpaysimplificado/dtos/transactionsDtos/GetTransactionDTO.java
@@ -0,0 +1,23 @@
+package com.br.picpaysimplificado.dtos.transactionsDtos;
+
+import com.br.picpaysimplificado.domain.transactions.Transaction;
+import com.br.picpaysimplificado.domain.users.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record GetTransactionDTO(
+        Long id,
+        BigDecimal amount,
+        User sender,
+        User receiver,
+        LocalDateTime timestamp
+) {
+    public GetTransactionDTO(Transaction transaction){
+        this(transaction.getId(),
+                transaction.getAmount(),
+                transaction.getSender(),
+                transaction.getReceiver(),
+                transaction.getTimestamp());
+    }
+}
