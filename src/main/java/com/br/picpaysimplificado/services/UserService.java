e93095ef25bc6541d293111fe4cb78c2d76e8770
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 18:06:13 -0300

    UserService finalizada

--- src/main/java/com/br/picpaysimplificado/services/UserService.java
+++ src/main/java/com/br/picpaysimplificado/services/UserService.java
@@ -2,11 +2,16 @@ package com.br.picpaysimplificado.services;
 
 import com.br.picpaysimplificado.domain.users.User;
 import com.br.picpaysimplificado.domain.users.UserType;
+import com.br.picpaysimplificado.dtos.usersDTOs.CreateUserDTO;
+import com.br.picpaysimplificado.dtos.usersDTOs.GetUserDTO;
+import com.br.picpaysimplificado.infra.exceptions.NotFoundException;
 import com.br.picpaysimplificado.infra.exceptions.ValidateException;
 import com.br.picpaysimplificado.repositories.UserRepositorie;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -19,5 +24,26 @@ public class UserService {
         } if (sender.getBalance().compareTo(amount) < 0){
             throw new ValidateException("Saldo insuficiente.");
         }
+    }
+
+    public User findUserByCPF(String cpf){
+        return this.repositorie.findUserByCPF(cpf).orElseThrow(NotFoundException::new);
+    }
+
+    public void updateUser(User user){
+        this.repositorie.save(user);
+    }
+
+    public GetUserDTO createUser(CreateUserDTO userDTO){
+        User user = new User(userDTO);
+        this.repositorie.save(user);
+        return new GetUserDTO(user);
+    }
+
+    public List<GetUserDTO> findAllUsers(){
+        return this.repositorie.findAll()
+                .stream()
+                .map(GetUserDTO::new)
+                .collect(Collectors.toList());
     }
 }
