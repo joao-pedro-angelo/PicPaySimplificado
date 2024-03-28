f9a5fbe488ac941a1d4872095b05d37dcd85d1c2
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 17:49:12 -0300

    cria repositório de usuários

---
+++ src/main/java/com/br/picpaysimplificado/repositories/UserRepositorie.java
@@ -0,0 +1,12 @@
+package com.br.picpaysimplificado.repositories;
+
+import com.br.picpaysimplificado.domain.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepositorie extends JpaRepository<User, Long> {
+    Optional<User> findUserByCPF(String cpf);
+}
