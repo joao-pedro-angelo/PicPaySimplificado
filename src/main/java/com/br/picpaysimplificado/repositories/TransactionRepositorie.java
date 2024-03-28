1515d79c15f6448e98993d40bf508daf0064e10a
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-28 17:50:07 -0300

    cria repositório de transações

---
+++ src/main/java/com/br/picpaysimplificado/repositories/TransactionRepositorie.java
@@ -0,0 +1,10 @@
+package com.br.picpaysimplificado.repositories;
+
+import com.br.picpaysimplificado.domain.transactions.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TransactionRepositorie extends JpaRepository<Transaction, Long> {
+    Optional<Transaction> findTransactionById(Long id);
+}
